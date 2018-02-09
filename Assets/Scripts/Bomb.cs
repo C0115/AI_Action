d927835cd967a5dc9da855ef9cf29945a457fd59
EvAndrO <c0115178d1@edu.teu.ac.jp>
2018-02-09 19:26:04 +0900

    プレイヤーから定期的に弾（爆弾）が発射される

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+
+	public int speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D>().velocity = transform.right.normalized * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	//PlayerBombのプレハブ
+	public GameObject bomb;
+
+	//Startメソッドをコルーチンとして呼び出す
+	// Use this for initialization
+	IEnumerator Start () {
+		while(true){
+			//if (Input.GetKey (KeyCode.Space)) {
+				//爆弾をプレイヤーと同じ位置/角度で呼び出す
+				Instantiate (bomb, transform.position, transform.rotation);
+				//0.5秒待つ
+				yield return new WaitForSeconds (0.5f);
+			//}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
