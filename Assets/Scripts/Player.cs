2160ed3a3a92dfe9f0f2497748a1b32fa360e40c
EvAndrO <c0115178d1@edu.teu.ac.jp>
2018-02-09 19:58:30 +0900

    スペースキーを押すと弾を撃つ

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,23 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	//弾を撃ったか
+	public bool IsBomb = false;
+	public int BombCount = 0;
 	//PlayerBombのプレハブ
 	public GameObject bomb;
 
 	//Startメソッドをコルーチンとして呼び出す
 	// Use this for initialization
-	IEnumerator Start () {
-		while(true){
-			//if (Input.GetKey (KeyCode.Space)) {
-				//爆弾をプレイヤーと同じ位置/角度で呼び出す
-				Instantiate (bomb, transform.position, transform.rotation);
-				//0.5秒待つ
-				yield return new WaitForSeconds (0.5f);
-			//}
-		}
+	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(IsBomb){
+			BombCount++;
+			if(BombCount >= 50){
+				IsBomb = false;
+				BombCount = 0;
+			}
+		}else if (Input.GetKeyDown (KeyCode.Space)) {
+			//爆弾をプレイヤーと同じ位置/角度で呼び出す
+			Instantiate (bomb, transform.position, transform.rotation);
+			//IsBombをtrueに
+			IsBomb = true;
+			//0.5秒待つ
+			//yield return new WaitForSeconds (0.5f);
+		}
 	}
 }
